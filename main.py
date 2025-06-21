fecfff557742b5b82204ae1135b3d91cb6e46542
piot3k <grodzki.p.pg@gmail.com>
2025-06-21 19:27:13 +0200

    wip: preparet remove_uder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from utils.controller import get_user_info, add_user
+from utils.controller import get_user_info, add_user, remove_user
 from utils.model import users
+
 
 def main():
     while True:
@@ -12,12 +13,10 @@ def main():
         print("==============MENU==============")
 
         choice = input('Wybierz opcję menu: ')
-        if choice == '0':
-            break
-        if choice == '1':
-            get_user_info(users)
+        if choice == '0': break
+        if choice == '1': get_user_info(users)
         if choice == '2': add_user(users)
-
+        if choice == '3': remove_user(users)
 
 
 if __name__ == '__main__':
--- utils/controller.py
+++ utils/controller.py
@@ -8,3 +8,11 @@ def add_user(users_data: list) -> None:
     new_location = input('Podaj lokalizację nowego użytkownika: ')
     new_posts = int(input('Podaj liczbę postów nowego użytkownika: '))
     users_data.append({"name": new_name, "location": new_location, "posts": new_posts})
+
+def remove_user(users_data: list) -> None:
+    uzytkownik_do_usuniecia = input('Podaj użytkownika do usunięcia: ')
+
+    for user in users_data:
+        if user['name'] == uzytkownik_do_usuniecia:
+            users_data.remove(user)
+
